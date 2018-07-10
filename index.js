7ee573a82dae6956e3ad9a0a60704fda412a3586
Patricia Carter <patricia4194@gmail.com>
2018-07-10 19:27:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
-    return 'I love you too'
+    return 'I love you, too'
   } else if (string === string.toUpperCase()) {
     return 'YES INDEED!'
   } else if (string === string.toLowerCase()) {
