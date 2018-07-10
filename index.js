7d4726437e96446fd31d860c774807d5bd3ce5a5
Patricia Carter <patricia4194@gmail.com>
2018-07-10 19:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  ${string}.toUpperCase()
-  console.log(`${string}`)
+  ${string}.toUpperCase();
+  console.log(`${string}`);
 }
 
 logWhisper() {
